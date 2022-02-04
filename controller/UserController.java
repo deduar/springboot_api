8a91aec54137e48956581744ce048664f1b98e6a
deduar <deduar@gmail.com>
2022-02-04 21:42:23 +0100

    update, delete all

--- controller/UserController.java
+++ controller/UserController.java
@@ -1,6 +1,9 @@
 package com.deduar.sbt.controller;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import com.deduar.sbt.entity.User;
 import com.deduar.sbt.service.UserService;
@@ -8,9 +11,11 @@ import com.deduar.sbt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,18 +29,51 @@ public class UserController {
 
     // Create a new user
     @PostMapping
-    public ResponseEntity<?> create(@RequestBody User user){
+    public ResponseEntity<?> create(@RequestBody User user) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user));
     }
-    
+
     // Read an user
     @GetMapping("/{id}")
-    public ResponseEntity<?> read(@PathVariable(value = "id") Long userId){
+    public ResponseEntity<?> read(@PathVariable(value = "id") Long userId) {
         Optional<User> oUser = userService.findById(userId);
-        if(!oUser.isPresent()){
+        if (!oUser.isPresent()) {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok(oUser);
     }
+    
+    // Update an user
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@RequestBody User userDetails, @PathVariable(value = "id") Long userId){
+        Optional<User> user = userService.findById(userId);
+        if(!user.isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        user.get().setName(userDetails.getName());
+        user.get().setSurename(userDetails.getSurename());
+        user.get().setEmail(userDetails.getEmail());
+        user.get().setEnable(userDetails.getEnable());
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.save(user.get()));
+    }
 
+    // Delete an user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long userId){
+        if(!userService.findById(userId).isPresent()){
+            return ResponseEntity.notFound().build();
+        }
+        userService.deleteById(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    // Read all users
+    @GetMapping
+    public List<User> readAll() {
+        List<User> users = StreamSupport
+            .stream(userService.findAll().spliterator(), false)
+            .collect(Collectors.toList());
+
+        return users;
+    }
 }
